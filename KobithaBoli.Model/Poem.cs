c05d75d1ff9960c05efaffc7f23fb4292b860c27
RanjanBiswasFedJobs <ranjanbiswasfedjobs@gmail.com>
2015-02-21 18:23:25 -0500

    Model Classe and EF
    Adding model classes and EF

---
+++ KobithaBoli.Model/Poem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KobithaBoli.Model
+{
+    public class Poem
+    {
+        public int PoemId { get; set; }
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        [Column(TypeName = "ntext")]
+        [MaxLength]
+        public string Contennt { get; set; } 
+
+
+
+    }
+}
