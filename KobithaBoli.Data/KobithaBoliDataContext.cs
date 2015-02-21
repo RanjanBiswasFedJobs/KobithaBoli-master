83c634c8ec460c2e3657246f0dafdf68c297c61d
RanjanBiswasFedJobs <ranjanbiswasfedjobs@gmail.com>
2015-02-21 18:57:47 -0500

    Kobitha Boli
    KobithaBoli updates.

---
+++ KobithaBoli.Data/KobithaBoliDataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KobithaBoli.Data
+{
+    public class KobithaBoliDataContext : DbContext
+    {
+
+         static KobithaBoliDataContext()
+        {
+           // Database.SetInitializer(new CodeCamperDatabaseInitializer());
+        }
+
+
+         public KobithaBoliDataContext()
+            : base(nameOrConnectionString: "KobithaBoli") { }
+
+
+
+    }
+}
